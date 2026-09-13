7d2ee01f5d166c7d6b787dd5c9ed4d8caad831b6
Katy1690 <katherine.gonzalez.s@alumno.iplacex.cl>
2026-09-13 20:46:13 -0300

    Actualizando archivos y corrección de autenticación

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ async function autenticar() {
   }
 
   try {
-    const res = await fetch('https://proyecto-frontel.onrender.com/api/login', {
+    const res = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
